19317aeeac281e209ff540737c05722fd8ca4d10
Philip Brohan <philip@brohan.org>
2021-06-21 15:26:08 +0100

    Added script to make daily Tmax from UKPP hourlies.

--- get_operational_data/UKPP/get_day_from_MASS.py
+++ get_operational_data/UKPP/get_day_from_MASS.py
@@ -25,7 +25,7 @@ args = parser.parse_args()
 
 moose_dir = "moose:/opfc/atm/postpro/prods/%04d.tar" % args.year
 
-local_dir = "%s/Proxy_Hadobs/opfc/UKPP/%04d%02d%01d" % (
+local_dir = "%s/Proxy_Hadobs/opfc/UKPP/%04d%02d%02d" % (
     os.getenv("SCRATCH"),
     args.year,
     args.month,
@@ -34,9 +34,16 @@ local_dir = "%s/Proxy_Hadobs/opfc/UKPP/%04d%02d%01d" % (
 if not os.path.isdir(local_dir):
     os.makedirs(local_dir)
 
+
 def unarchive(year, month, day):
     for hour in range(0, 24):
-        tar_file = "%s/%04d%02d%02dT%02d30Z_UKP.tar" % (local_dir, year, month, day, hour)
+        tar_file = "%s/%04d%02d%02dT%02d30Z_UKP.tar" % (
+            local_dir,
+            year,
+            month,
+            day,
+            hour,
+        )
         moose.get(
             os.path.dirname(tar_file), "%s/%s" % (moose_dir, os.path.basename(tar_file))
         )
@@ -45,13 +52,13 @@ def unarchive(year, month, day):
     # Uncompress the data we want, delete the rest
     nfiles = os.listdir("%s" % local_dir)
     for nfile in nfiles:
-        #if nfile[-4:] == '.tar':
+        # if nfile[-4:] == '.tar':
         #    continue
-        if nfile[-17:] == 'temperature_2km.Z':
-            pret=subprocess.run("uncompress %s/%s" % (local_dir,nfile),shell=True)
+        if nfile[-17:] == "temperature_2km.Z":
+            pret = subprocess.run("uncompress %s/%s" % (local_dir, nfile), shell=True)
             pret.check_returncode()
         else:
-            os.remove("%s/%s" % (local_dir,nfile))
+            os.remove("%s/%s" % (local_dir, nfile))
 
 
-unarchive(args.year,args.month,args.day)
+unarchive(args.year, args.month, args.day)
