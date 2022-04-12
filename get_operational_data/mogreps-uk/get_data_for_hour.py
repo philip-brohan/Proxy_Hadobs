a1230ed04ed022bf191e513f2203e59fa66c5f02
Philip Brohan <philip@brohan.org>
2022-04-12 17:59:24 +0100

    Updated mogreps-uk mass extraction scripts to group by validity time.

--- get_operational_data/mogreps-uk/get_data_for_hour.py
+++ get_operational_data/mogreps-uk/get_data_for_hour.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 # Retrieve surface weather variables from the mogreps-uk analysis
-#  for one day.
+#  for one hour.
+# Gets data from all available forecasts for one given validity time.
 
 import os
 import sys
 from tempfile import NamedTemporaryFile
 import subprocess
+import datetime
 
 import argparse
 
@@ -16,7 +18,10 @@ parser.add_argument("--month", help="Integer month", type=int, required=True)
 parser.add_argument("--day", help="Day of month", type=int, required=True)
 parser.add_argument("--hour", help="Hour of day", type=int, required=True)
 parser.add_argument(
-    "--lead", help="lead time (hours)", type=int, required=False, default=3
+    "--max_lead", help="max lead time (hours)", type=int, required=False, default=130
+)
+parser.add_argument(
+    "--min_lead", help="min lead time (hours)", type=int, required=False, default=0
 )
 parser.add_argument(
     "--opdir",
@@ -24,44 +29,99 @@ parser.add_argument(
     default="%s/opfc/mogreps-uk" % os.getenv("SCRATCH"),
 )
 args = parser.parse_args()
-opdir = "%s/%04d/%02d" % (args.opdir, args.year, args.month)
+opdir = "%s/%04d/%02d/%02d" % (args.opdir, args.year, args.month, args.day)
 if not os.path.isdir(opdir):
     os.makedirs(opdir)
 
 print("%04d-%02d-%02d:%02d" % (args.year, args.month, args.day, args.hour))
 
-# Mass directory to use
-mass_dir = "moose:/opfc/atm/mogreps-uk/prods/%04d%02d.pp" % (args.year, args.month)
+target_d = datetime.datetime(args.year, args.month, args.day, args.hour)
 
-# Files to retrieve from
-flist = []
-for member in range(34):
-    hour = args.hour
-    fcst = args.lead
-    flist.append(
-        "prods_op_mogreps-uk_%04d%02d%02d_%02d_%02d_%03d.pp"
-        % (args.year, args.month, args.day, hour, member, fcst)
+# Does a mass file contain data for the selected date
+def keep_file(fname):
+    lead = int(fname[-6:-3])
+    hour = int(fname[-12:-10])
+    day = int(fname[-15:-13])
+    month = int(fname[-17:-15])
+    year = int(fname[-21:-17])
+    rund = datetime.datetime(year, month, day, hour)
+    leadh = int((target_d - rund).total_seconds() / 3600)
+    if (
+        leadh < args.max_lead
+        and leadh > args.min_lead
+        and leadh < lead
+        and leadh >= (lead - 3)
+    ):
+        return True
+    return False
+
+
+# Get all available files for given  month
+def files_for_month(year, month):
+    mr = subprocess.run(
+        ["moo", "ls", "moose:/opfc/atm/mogreps-uk/prods/%04d%02d.pp" % (year, month),],
+        capture_output=True,
+        text=True,
+    )
+    mr.check_returncode()  # Throw exception if moose call failed
+    mf = mr.stdout.split("\n")
+    flist = []
+    for fn in mf:
+        if len(fn) > 21 and keep_file(fn):
+            flist.append(fn)
+    return flist
+
+
+# Run a retrieval
+def run_retrieval(flist, mass_dir, opfile):
+    qfile = NamedTemporaryFile(mode="w+", delete=False)
+    qfile.write("begin_global\n")
+    qfile.write("   pp_file = (")
+    for ppfl in flist:
+        qfile.write('"%s"' % os.path.basename(ppfl))
+        if ppfl != flist[-1]:
+            qfile.write(",")
+        else:
+            qfile.write(")\n")
+    qfile.write("end_global\n")
+    qfile.write("begin\n")
+    qfile.write("    stash = (16222,3236,3225,3226,4203,4204)\n")
+    qfile.write("    lbproc = 0\n")
+    qfile.write("    yr = %d\n" % args.year)
+    qfile.write("    mon = %d\n" % args.month)
+    qfile.write("    day = %d\n" % args.day)
+    qfile.write("    hr = %d\n" % args.hour)
+    qfile.write("end\n")
+    qfile.close()
+
+    # Run the query
+    # opfile = "%s/%02d_%02d_%03d.pp" % (opdir, args.day, args.hour, args.lead)
+    subprocess.call(
+        "moo select -C %s %s %s" % (qfile.name, mass_dir, opfile), shell=True
     )
 
-# Create the query file
-qfile = NamedTemporaryFile(mode="w+", delete=False)
-qfile.write("begin_global\n")
-qfile.write("   pp_file = (")
-for ppfl in flist:
-    qfile.write('"%s"' % ppfl)
-    if ppfl != flist[-1]:
-        qfile.write(",")
-    else:
-        qfile.write(")\n")
-qfile.write("end_global\n")
-qfile.write("begin\n")
-qfile.write("    stash = (16222,3236,3225,3226,4203,4204)\n")
-qfile.write("    lbproc = 0\n")
-qfile.write("end\n")
-qfile.close()
+    os.remove(qfile.name)
 
-# Run the query
-opfile = "%s/%02d_%02d_%03d.pp" % (opdir, args.day, args.hour, args.lead)
-subprocess.call("moo select -C %s %s %s" % (qfile.name, mass_dir, opfile), shell=True)
 
-os.remove(qfile.name)
+# Get all data archived in the current month's directory
+flist = files_for_month(args.year, args.month)
+if len(flist) > 0:
+    run_retrieval(
+        flist,
+        "moose:/opfc/atm/mogreps-uk/prods/%04d%02d.pp" % (args.year, args.month),
+        "%s/%02d_%03d_%03d_c.pp" % (opdir, args.hour, args.min_lead, args.max_lead),
+    )
+mlead = (args.day - 1) * 24 + args.hour
+if args.max_lead > mlead:
+    year = args.year
+    month = args.month - 1
+    if month < 1:
+        year -= 1
+        month = 12
+    flist = files_for_month(year, month)
+    if len(flist) > 0:
+        run_retrieval(
+            flist,
+            "moose:/opfc/atm/mogreps-uk/prods/%04d%02d.pp" % (year, month),
+            "%s/%02d_%03d_%03d_p.pp" % (opdir, args.hour, args.min_lead, args.max_lead),
+        )
--- get_operational_data/mogreps-uk/get_data_for_period.py
+++ get_operational_data/mogreps-uk/get_data_for_period.py
@@ -15,7 +15,8 @@ parser.add_argument("--endyear", type=int, required=True)
 parser.add_argument("--endmonth", type=int, required=True)
 parser.add_argument("--endday", type=int, required=True)
 parser.add_argument("--endhour", type=int, required=False, default=23)
-parser.add_argument("--lead", type=int, required=False, default=3)
+parser.add_argument("--max_lead", type=int, required=False, default=6)
+parser.add_argument("--min_lead", type=int, required=False, default=0)
 args = parser.parse_args()
 
 current = datetime.datetime(
@@ -24,17 +25,28 @@ current = datetime.datetime(
 endd = datetime.datetime(args.endyear, args.endmonth, args.endday, args.endhour, 59, 59)
 
 while current <= endd:
-    opfile = "%s/opfc/mogreps-uk/%04d/%02d/%02d_%02d_%03d.pp" % (
+    opfile = "%s/opfc/mogreps-uk/%04d/%02d/%02d/%02d_%03d_%03d_c.pp" % (
         os.getenv("SCRATCH"),
         current.year,
         current.month,
         current.day,
         current.hour,
-        args.lead,
+        args.min_lead,
+        args.max_lead,
     )
     if not os.path.isfile(opfile):
         print(
-            "./get_data_for_hour.py --year=%d --month=%d --day=%d --hour=%d --lead=%d"
-            % (current.year, current.month, current.day, current.hour, args.lead)
+            (
+                "./get_data_for_hour.py --year=%d --month=%d --day=%d --hour=%d "
+                + "--min_lead=%d --max_lead=%d"
+            )
+            % (
+                current.year,
+                current.month,
+                current.day,
+                current.hour,
+                args.min_lead,
+                args.max_lead,
+            )
         )
     current += datetime.timedelta(hours=1)
